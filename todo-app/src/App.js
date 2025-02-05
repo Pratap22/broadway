dcc0d743b60e035dce94e64b814c9cd28374df5c
Pratap Sharma <sharma.pratap22@gmail.com>
2025-02-05 21:21:32 +0545

    Completed todo using class

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -3,18 +3,18 @@ import Header from "./components/Header";
 import Tabs from "./components/Tabs";
 import TextInput from "./components/TextInput";
 import TodoItem from "./components/TodoItem";
+import deleteIcon from "./assets/delete.png";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       selectedKey: "personal",
-      text: "",
-      todos: [
-        { title: "Professional Work No. 1", completed: false },
-        { title: "Professional Work No. 3", completed: true },
-        { title: "Professional Work No. 4", completed: false },
-      ],
+      todoItem: {
+        title: "",
+        completed: false,
+      },
+      todos: [{ title: 1, completed: false, _id: 1 }],
     };
   }
 
@@ -23,17 +23,64 @@ class App extends React.Component {
   };
 
   addNewTodo = () => {
-    const { text, todos } = this.state;
-    if (!text) {
+    const { todoItem, todos } = this.state;
+    if (!todoItem.title) {
       return "";
     }
-    const newTodo = { title: text, completed: false };
-    todos.push(newTodo);
-    this.setState({ todos, text: "" });
+    if (todoItem._id) {
+      this.setState({
+        todos: todos.map((todo) => {
+          if (todo._id === todoItem._id) {
+            todo = todoItem;
+          }
+          return todo;
+        }),
+        todoItem: { title: "", completed: false },
+      });
+    } else {
+      const newTodo = { ...todoItem, _id: Date.now() };
+      todos.push(newTodo);
+      this.setState({ todos, todoItem: { title: "", completed: false } });
+    }
   };
 
+  onToggle = (id) => {
+    const { todos } = this.state;
+
+    this.setState({
+      todos: todos.map((todo) => {
+        if (todo._id === id) {
+          todo.completed = !todo.completed;
+        }
+
+        return todo;
+      }),
+    });
+  };
+
+  onDelete = (id) => {
+    const { todos } = this.state;
+
+    this.setState({
+      todos: todos.filter((todo) => {
+        return todo._id !== id;
+      }),
+    });
+  };
+
+  onRowClick = (id) => {
+    const { todos } = this.state;
+    const currItem = todos.find((todo) => todo._id === id);
+    this.setState({ todoItem: currItem });
+  };
+
+  clearCompleted = () => {
+    const { todos } = this.state;
+    this.setState({ todos: todos.filter(todo => !todo.completed) });
+  }
+
   render() {
-    const { selectedKey, text, todos } = this.state;
+    const { selectedKey, todoItem, todos } = this.state;
     return (
       <div>
         <Header />
@@ -48,19 +95,38 @@ class App extends React.Component {
         <div className="px-48">
           <TextInput
             placeholder="What do you need to do?"
-            buttonText="Add"
+            buttonText={todoItem._id ? "Update" : "Add"}
             onClick={this.addNewTodo}
-            onChange={(text) => this.setState({ text })}
-            value={text}
+            onChange={(text) =>
+              this.setState({ todoItem: { ...todoItem, title: text } })
+            }
+            value={todoItem.title}
           />
           <div className="bg-app-50 mt-6 rounded-3xl">
-            {todos.map((todo, index) => (
-              <TodoItem
-                key={index}
-                text={todo.title}
-                checked={todo.completed}
-              />
-            ))}
+            {todos.length > 0 ? (
+              <>
+                {todos.map((todo, index) => (
+                  <TodoItem
+                    key={index}
+                    text={todo.title}
+                    checked={todo.completed}
+                    onToggle={() => this.onToggle(todo._id)}
+                    onDelete={() => this.onDelete(todo._id)}
+                    onRowClick={() => this.onRowClick(todo._id)}
+                  />
+                ))}
+                <div className="pb-2 pt-4">
+                  <div className="flex justify-end mr-4" onClick={this.clearCompleted}>
+                    <img alt="clear" src={deleteIcon} className="w-6" />
+                    <p className="ml-2 text-app">Clear Completed</p>
+                  </div>
+                </div>
+              </>
+            ) : (
+              <div className="p-4 text-center">
+                No todos found. Please add new todo.
+              </div>
+            )}
           </div>
         </div>
       </div>
--- todo-app/src/components/TodoItem.jsx
+++ todo-app/src/components/TodoItem.jsx
@@ -2,7 +2,7 @@ import circle from "../assets/circle.png";
 import check from "../assets/check.png";
 import deleteIcon from "../assets/delete.png";
 
-const TodoItem = ({ checked, text }) => {
+const TodoItem = ({ checked, text, onToggle, onDelete, onRowClick }) => {
   return (
     <div className="pb-2">
       <div className="flex items-center">
@@ -10,14 +10,23 @@ const TodoItem = ({ checked, text }) => {
           className="w-1/12 p-4"
           alt="circle"
           src={checked ? check : circle}
+          onClick={onToggle}
         />
-        <p className="w-10/12 align-middle text-3xl text-app-fade">{text}</p>
+        <p
+          className={`w-10/12 align-middle text-3xl text-app-fade ${
+            checked ? "line-through" : ""
+          }`}
+          onClick={onRowClick}
+        >
+          {text}
+        </p>
         <img
           className="p-4"
           height={64}
           width={64}
           alt="circle"
           src={deleteIcon}
+          onClick={onDelete}
         />
       </div>
       <div className="border-2 border-app-blue mx-12" />
